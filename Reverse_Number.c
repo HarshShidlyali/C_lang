ce73b94a141a502f2468d61335ea28b7a05c9ecc
Harsh Shidlyali <135652215+HarshShidlyali@users.noreply.github.com>
2025-08-20 22:16:21 +0530

    Reverse_Number.c

--- Reverse_Number.c
+++ Reverse_Number.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main()
+{
+    int n,r,sum=0;
+    printf("Enter the number to reverse : ");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        sum=sum*10+r;
+        n=n/10;
+    }
+    printf("Reversed number is : %d",sum);
+}
